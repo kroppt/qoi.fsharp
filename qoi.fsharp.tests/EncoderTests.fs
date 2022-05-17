a9016267ada4636c78b983fd8d449ae1f566079f
Tyler Kropp <tyler@tylerkropp.xyz>
2022-05-17 16:59:08 -0400

    Add correct header test

--- qoi.fsharp.tests/EncoderTests.fs
+++ qoi.fsharp.tests/EncoderTests.fs
@@ -2,9 +2,52 @@ module EncoderTests
 
 open Xunit
 open Qoi.Fsharp.Encoder
+open System.IO
+open System
 
 [<Fact>]
 let ``Should succeed`` () =
     let input = [ 0uy; 0uy; 0uy; 255uy ]
+    let width = 1
+    let height = 1
+    let channels = Channels.Rgba
+    let colorSpace = ColorSpace.SRgb
 
-    Encoder.Encode input |> ignore
+    Encoder.Encode(input, width, height, channels, colorSpace)
+    |> ignore
+
+[<Fact>]
+let ``Should have correct header`` () =
+    let input = []
+
+    let writeBigEndian (binWriter: BinaryWriter) (value: int) =
+        binWriter.Write(byte ((value >>> 24) &&& 0xFF))
+        binWriter.Write(byte ((value >>> 16) &&& 0xFF))
+        binWriter.Write(byte ((value >>> 8) &&& 0xFF))
+        binWriter.Write(byte ((value >>> 0) &&& 0xFF))
+
+    let width = 1
+    let height = 1
+    let channels = Channels.Rgba
+    let colorSpace = ColorSpace.SRgb
+
+    let expected =
+        using (new MemoryStream()) (fun memStream ->
+            using (new BinaryWriter(memStream)) (fun binWriter ->
+                binWriter.Write(byte 'q')
+                binWriter.Write(byte 'o')
+                binWriter.Write(byte 'i')
+                binWriter.Write(byte 'f')
+                writeBigEndian binWriter width
+                writeBigEndian binWriter height
+                binWriter.Write(byte channels)
+                binWriter.Write(byte colorSpace))
+
+            memStream.ToArray())
+
+    let input = [ 0uy; 0uy; 0uy; 255uy ]
+
+    let bytes = Encoder.Encode(input, width, height, channels, colorSpace)
+
+    let actual = new ArraySegment<byte>(bytes, 0, 14)
+    Assert.Equal(expected, actual)
--- qoi.fsharp/Encoder.fs
+++ qoi.fsharp/Encoder.fs
@@ -1,7 +1,66 @@
 ﻿namespace Qoi.Fsharp
 
 module Encoder =
-    exception Abc of string
+    open System.IO
+
+    type public Channels =
+        | Rgb = 3
+        | Rgba = 4
+
+    type public ColorSpace =
+        | SRgb = 0
+        | Linear = 1
 
     type public Encoder =
-        static member Encode(input: byte list) : byte list = []
+        private new(binWriter: BinaryWriter,
+                    input: byte list,
+                    width: int,
+                    height: int,
+                    channels: Channels,
+                    colorSpace: ColorSpace) =
+            { binWriter = binWriter
+              input = input
+              width = width
+              height = height
+              channels = channels
+              colorSpace = colorSpace }
+
+        val binWriter: BinaryWriter
+        val input: byte list
+        val width: int
+        val height: int
+        val channels: Channels
+        val colorSpace: ColorSpace
+
+        static member public Encode
+            (
+                input: byte list,
+                width: int,
+                height: int,
+                channels: Channels,
+                colorSpace: ColorSpace
+            ) : byte [] =
+            using (new MemoryStream()) (fun memStream ->
+                using (new BinaryWriter(memStream)) (fun binWriter ->
+                    let encoder = new Encoder(binWriter, input, width, height, channels, colorSpace)
+                    encoder.Encode())
+
+                memStream.ToArray())
+
+        member private this.writeBigEndian(value: int) =
+            this.binWriter.Write(byte ((value >>> 24) &&& 0xFF))
+            this.binWriter.Write(byte ((value >>> 16) &&& 0xFF))
+            this.binWriter.Write(byte ((value >>> 8) &&& 0xFF))
+            this.binWriter.Write(byte ((value >>> 0) &&& 0xFF))
+
+        member private this.WriteHeader() =
+            this.binWriter.Write(byte 'q')
+            this.binWriter.Write(byte 'o')
+            this.binWriter.Write(byte 'i')
+            this.binWriter.Write(byte 'f')
+            this.writeBigEndian (this.width)
+            this.writeBigEndian (this.height)
+            this.binWriter.Write(byte this.channels)
+            this.binWriter.Write(byte this.colorSpace)
+
+        member private this.Encode() = this.WriteHeader()

6263f9c646d784207ca5f2443793480b682a8662
Tyler Kropp <tyler@tylerkropp.xyz>
2022-05-23 14:06:28 -0400

    Add bad magic bytes decoder test

--- qoi.fsharp.tests/DecoderTests.fs
+++ qoi.fsharp.tests/DecoderTests.fs
@@ -6,6 +6,8 @@ open Qoi.Fsharp.Header
 
 [<Fact>]
 let ``Should succeed`` () =
+    let expected = DecodeResult.Ok
+
     let input =
         [ byte 'q'
           byte 'o'
@@ -35,4 +37,43 @@ let ``Should succeed`` () =
           0uy
           1uy ]
 
-    Decode input |> ignore
+    let actual = Decode input
+
+    Assert.Equal(expected, actual)
+
+[<Fact>]
+let ``Should fail parsing bad magic bytes`` () =
+    let expected = DecodeResult.BadMagicBytes
+
+    let input =
+        [ byte 'a'
+          byte 'b'
+          byte 'c'
+          byte 'd'
+
+          0uy
+          0uy
+          0uy
+          0uy
+
+          0uy
+          0uy
+          0uy
+          0uy
+
+          byte Channels.Rgb
+
+          byte ColorSpace.SRgb
+
+          0uy
+          0uy
+          0uy
+          0uy
+          0uy
+          0uy
+          0uy
+          1uy ]
+
+    let actual = Decode input
+
+    Assert.Equal(expected, actual)
--- qoi.fsharp/Decoder.fs
+++ qoi.fsharp/Decoder.fs
@@ -1,4 +1,21 @@
 namespace Qoi.Fsharp
 
 module Decoder =
-    let public Decode (input: byte list) = ()
+    [<RequireQualifiedAccess>]
+    type public DecodeResult =
+        | Ok
+        | BadMagicBytes
+
+    let public Decode (input: byte list) =
+        let correctMagic =
+            [ byte 'q'
+              byte 'o'
+              byte 'i'
+              byte 'f' ]
+
+        let actualMagic = List.take 4 input
+
+        if actualMagic = correctMagic then
+            DecodeResult.Ok
+        else
+            DecodeResult.BadMagicBytes
